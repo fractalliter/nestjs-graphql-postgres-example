2e8ba5a4e54d4c0fa9e7a71257c9ecc10e76f4e4
Elias Rahmani <eliass.rahmani@gmail.com>
2021-05-14 23:27:29 +0430

    resolve query join

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -21,7 +21,7 @@ export class EmployeeService {
             extract(Month from orders."Order Date"::date) as monthly,
             sum(v."Voucher Amount") as spend
             from public.orders
-                left join employees e on orders."Employee ID" = e."Employee ID"
+                right join employees e on orders."Employee ID" = e."Employee ID"
                 left join vouchers v on orders."Voucher ID" = v."Voucher ID"
             group by monthly ,e."Company ID", e."Company Title",orders."Employee ID", e."Employee Name",e."Monthly Budget"
             ) as l
@@ -46,7 +46,7 @@ export class EmployeeService {
                   extract(Month from orders."Order Date"::date) as monthly,
                   sum(v."Voucher Amount") as total
            from public.orders
-                    left join employees e on orders."Employee ID" = e."Employee ID"
+                    right join employees e on orders."Employee ID" = e."Employee ID"
                     left join vouchers v on orders."Voucher ID" = v."Voucher ID"
               group by monthly ,e."Company ID", e."Company Title",orders."Employee ID", e."Employee Name",e."Monthly Budget"
         ) as l
