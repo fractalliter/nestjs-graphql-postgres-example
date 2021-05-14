d15485391264218c98ed5aa93fee91a33766898b
Elias Rahmani <eliass.rahmani@gmail.com>
2021-05-14 18:49:30 +0430

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmployeeResolver } from './employee/employee.resolver';
+import { EmployeeService } from './employee/employee.service';
+import { VoucherModel } from './voucher/voucher.model';
+import { OrderService } from './order/order.service';
+import { OrderResolver } from './order/order.resolver';
+import { VoucherService } from './voucher/voucher.service';
+import { VoucherResolver } from './voucher/voucher.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: ['dist/**/*.model.js'],
+      synchronize: false,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    EmployeeService,
+    EmployeeResolver,
+    OrderService,
+    OrderResolver,
+    VoucherService,
+    VoucherResolver,
+  ],
+})
+export class AppModule {}
